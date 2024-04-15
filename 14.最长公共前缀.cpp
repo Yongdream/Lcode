190a7b1e664a4494c3c0a8bea1b5f46145b4ea04
yongdream <youngzerooo@126.com>
2024-04-15 22:31:04 +0800

    14

--- 14.最长公共前缀.cpp
+++ 14.最长公共前缀.cpp
@@ -9,15 +9,15 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.empty()) return "";
-
         string ans = strs[0];
         int ansNum = -1;
         for (int i = 0; i < strs.size(); i++){       // 几个字符串
-                int minLen = min(ans.length(), strs[i].length()); 
-                for (int j = 0; j < minLen; j++){       // 判断前缀
-                    if (strs[i][j] != ans[j]) break;
-                    ansNum = j;
-                }
+            ansNum = -1;
+            int minLen = min(ans.length(), strs[i].length()); 
+            for (int j = 0; j < minLen; j++){       // 判断前缀
+                if (strs[i][j] != ans[j]) break;
+                ansNum = j;
+            }
         }
         ans = ans.substr(0, ansNum+1);
         if (ansNum == -1){
