ee664280fa0f201fe0805dbb92975bbfa366f74c
yongdream <youngzerooo@126.com>
2024-04-19 21:31:08 +0800

    15.去重解决

--- 15.三数之和.cpp
+++ 15.三数之和.cpp
@@ -8,32 +8,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        // for (const int& num : nums )
-        // {
-        //     cout << num << endl;
-        // }
-        
-        int right = nums.size() - 1;
-        int left = 0;
         vector<vector<int>> res;
+        if (nums.size() < 3) return res; 
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
 
-        while (right > left)
-        {
-            for(int k = left + 1 ; k < right - 1; ++k)
-            {
-                long long sum = static_cast<long long>(nums[k]) + nums[left] + nums[right];
+        for (int i = 0; i < n-2; ++i){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;  // 去重
+            
+            int left = i + 1, right = n - 1;    // 初始化左右指针
+            while (right > left){
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0)
                 {
-                    res.push_back({nums[k], nums[left], nums[right]});
-                    // 跳过相同的元素
-                    while (left < right && nums[left] == nums[left + 1]) ++left;
-                    while (left < right && nums[right] == nums[right - 1]) --right;
+                    res.push_back({nums[i], nums[left++], nums[right--]});
+                    // res.push_back({nums[i], nums[left], nums[right]});
+                    
+                    // 去重
+                    while (left < right && nums[left] == nums[left - 1]) ++left;
+                    while (left < right && nums[right] == nums[right + 1]) --right;
                 }
                 else if (sum < 0) ++left;
                 else --right;
-                
             }
+
         }
         return res;
     }
