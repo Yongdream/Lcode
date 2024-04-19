6265975744057275a6b404a045fa36b8f7b366d6
yongdream <youngzerooo@126.com>
2024-04-19 13:58:06 +0800

    49

--- 49.字母异位词分组.cpp
+++ 49.字母异位词分组.cpp
@@ -8,13 +8,15 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
+        unordered_map<string, vector<string>> mp;   // 以被排序后的string为key 以原string为value
         vector<vector<string>> res;
 
-        for (const string& str :strs){
+        for (const string& str : strs){
             string sortedStr = str;
             sort(sortedStr.begin(), sortedStr.end());
+            // cout <<  sortedStr << endl;
             mp[sortedStr].push_back(str);
+            // cout <<  "str:" <<str << endl;
         }
 
         for(const auto& pair : mp){
