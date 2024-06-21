682c067a0bdf9cdfc0f98f8b5b070ca763c79e5c
yongdream <youngzerooo@126.com>
2024-06-21 18:30:57 +0800

    637.二叉树的层平均值

--- 637.二叉树的层平均值.cpp
+++ 637.二叉树的层平均值.cpp
@@ -19,7 +19,25 @@
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> res;
+        queue<TreeNode*> que;
+        if (root != nullptr) que.push(root);
 
+        while (!que.empty()) 
+        {
+            int layerSize = que.size();
+            double sum = 0;
+            for (int i = 0; i < layerSize; ++i)
+            {
+                TreeNode* node = que.front();
+                que.pop();
+                sum += node->val;
+                if (node->left) que.push(node->left);   // 如果有值就加入队列维护
+                if (node->right) que.push(node->right);
+            }
+            res.push_back(sum / layerSize);
+        }
+        return res;
     }
 };
 // @lc code=end
