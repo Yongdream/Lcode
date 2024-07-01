38dec6a3649c951567fddbd64436c6c802d41149
yongdream <youngzerooo@126.com>
2024-07-01 17:03:45 +0800

    994 & 课程

---
+++ 210.课程表-ii.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=210 lang=cpp
+ *
+ * [210] 课程表 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> inDegree(numCourses, 0);
+        // prerequisites: [入度][出度]
+        // prerequisites[i][0] <--- prerequisites[i][1]
+        unordered_map<int, vector<int>> umap;
+        vector<int> res;
+
+        // 初始化入度数量 和 出度选择
+        for (int i = 0; i < prerequisites.size(); ++i)  
+        // 注意这里不选择numCourses prerequisites中会包含课程信息
+        {
+            inDegree[prerequisites[i][0]]++;    // 保存有几个先修课程（入度
+            umap[prerequisites[i][1]].push_back(prerequisites[i][0]); // 保存后续修课选中（出度
+        }
+
+        queue<int> que;
+        for (int i = 0; i < numCourses; ++i)
+        {
+            if (inDegree[i] == 0) que.push(i);  // 入度初始条件：不需要先修课程
+        }
+
+        int count = 0;
+        while (!que.empty())
+        {
+            int cur = que.front();
+            que.pop();
+            count++;
+            res.push_back(cur);
+
+            vector<int> courses = umap[cur];    // 后修课程
+            if (!courses.empty()){
+                for (int i = 0; i < courses.size(); ++i)
+                {
+                    inDegree[courses[i]]--;
+                    if (inDegree[courses[i]] == 0) que.push(courses[i]);
+                }
+            }
+        }
+        if (count == numCourses) return res;
+        return vector<int>();
+    }
+};
+// @lc code=end
+
--- 994.腐烂的橘子.cpp
+++ 994.腐烂的橘子.cpp
@@ -7,24 +7,66 @@
 // @lc code=start
 class Solution {
 private:
-    int row_;
-    int col_;
+    int row_, col_;
     int res_ = -1;
 
     queue<pair<int, int>> que_;
     
-    int dx_ = {0, 0, 1, -1};
-    int dy_ = {1, -1, 0, 0};
+    int dx_[4] = {0, 0, 1, -1};
+    int dy_[4] = {1, -1, 0, 0};
 
-    int bfs(vector<vector<int>>& grid, int x, int y){
-        que_.push({x, y});  // 1. 源点进队
-        grid[x][y] = 0;
+    void bfs(vector<vector<int>>& grid){
 
-        while (!que_.empty())
+        int freshCount = 0; // 统计新鲜橘子的数量
+
+        // 寻找所有腐烂橘子并初始化队列
+        for (int i = 0; i < row_; ++i)
         {
-
+            for (int j = 0; j < col_; ++j)
+            {
+                if (grid[i][j] == 2){
+                    que_.push({i, j});      // 初始化腐烂橘子
+                }
+                else if (grid[i][j] == 1){
+                    freshCount++;
+                }
+            }
         }
 
+        // 没有新鲜橘子 就不会存在腐烂过程
+        if (freshCount == 0) {
+            res_ = 0;
+            return;
+        }
+
+        int minutes = 1;
+        while (!que_.empty())
+        {
+            int size = que_.size();
+            for (int k = 0; k < size; ++k)
+            {
+                auto [x, y] = que_.front();
+                que_.pop();
+                for (int i = 0; i < 4; ++i) // 开始传染
+                {
+                    int newX = x + dx_[i];  //上下左右
+                    int newY = y + dy_[i];
+
+                    if (newX >= 0 && newX < row_ && // 边界
+                        newY >= 0 && newY < col_ && 
+                        grid[newX][newY] == 1   // 新鲜橘子
+                        )   
+                    {
+                        grid[newX][newY] = 2;   // 将新鲜橘子变为腐烂
+                        freshCount--;
+                        que_.push({newX, newY});
+                    }
+                }
+            }
+            if (freshCount > 0) minutes++; // 如果还有新鲜橘子，增加时间
+        }
+        // 如果所有橘子都腐烂了，返回分钟数，否则返回-1
+        res_ = freshCount == 0 ? minutes : -1;
     }
 
 
@@ -33,17 +75,8 @@ public:
         row_ = grid.size();
         col_ = grid[0].size();
 
-        for (int i = 0; i < row; ++i)
-        {
-            for (int j = 0; j < col; ++j)
-            {
-                if (grid[i][j] == 2)
-                {
-                    dfs(grid, x, y);
-                }
-            }
-        }
-
+        bfs(grid); // 执行BFS搜索
+        return res_;
     }
 };
 // @lc code=end
